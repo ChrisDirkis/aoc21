a51b3b1afc30825f4cbbb1cdeecd7703b292bc5b
Chris Dirkis <git@chrisdirk.is>
2021-12-01 19:48:57 +1100

    Day 1 zip version

--- day01.py
+++ day01.py
@@ -13,27 +13,15 @@ def part_1(filename):
     print(f"Part 1: {filename}")
     with open(filename) as file:
         ds = [int(line) for line in file]
-        count = 0
-        last = ds[0]
-        for d in ds[1:]:
-            if d > last:
-                count += 1
-            last = d
-        print(count)
+        print(len([1 for a, b in zip(ds, ds[1:]) if b > a]))
         
 
 def part_2(filename):
     print(f"Part 2: {filename}")
     with open(filename) as file:
         ds = [int(line) for line in file]
-        count = 0
-        last = ds[0] + ds[1] + ds[2]
-        for d1, d2, d3 in zip(ds[1:], ds[2:], ds[3:]):
-            s = d1 + d2 + d3
-            if s > last:
-                count += 1
-            last = s
-        print(count)
+        triples = [a + b + c for a, b, c in zip(ds, ds[1:], ds[2:])]
+        print(len([1 for a, b in zip(triples, triples[1:]) if b > a]))
 
 if __name__ == "__main__":
     part_1(testing_file_name)
